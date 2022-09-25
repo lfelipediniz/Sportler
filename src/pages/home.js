42fcc5aa7cc0e152694d491dc2fa1ebdbe5594f8
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2022-09-25 02:04:46 -0300

    Aplicação das fontes

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,19 +1,21 @@
-import React  from "react";
+import React from "react";
 
-import {View, StyleSheet} from 'react-native'
-
-export function Home(){
-
-    return(
-        <View style={styles.container}>
-
-        </View>
-    )
+import { View, Text, StyleSheet } from "react-native";
+import fonts from "../styles/fonts";
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>O que você quer jogar hoje?</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'cyan'
-    }
-  });
+  container: {
+    flex: 1,
+    backgroundColor: "cyan",
+  },
+  title: {
+    fontFamily: fonts.heading,
+  },
+});
